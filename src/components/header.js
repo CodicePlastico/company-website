8290e982ffdf62b8b592160b20836bc6b760d3a9
Andrea Gadaldi <ilgadaldi@gmail.com>
2020-06-30 14:23:52 +0200

    Footer & Header

--- src/components/header.js
+++ src/components/header.js
@@ -2,18 +2,18 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div>
-      <p>Logo</p>
-      <ul>
-        <li>menu item</li>
-        <li>menu item</li>
-        <li>menu item</li>
-      </ul>
-    </div>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+  return(
+    <header>
+        <img src="/images/codiceplastico_logo.svg"/>
+        <ul>
+          <li>menu item</li>
+          <li>menu item</li>
+          <li>menu item</li>
+        </ul>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Footer from "./footer"
 import "../scss/style.scss"
 
 const Layout = ({ children }) => {
@@ -30,12 +31,8 @@ const Layout = ({ children }) => {
         <main>{children}</main>
         <p className='gino'>Ciaone</p>
         <small>Small</small>
-        <footer>
-          <p>© {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a></p>
-        </footer>
       </div>
+      <Footer />
     </>
   )
 }

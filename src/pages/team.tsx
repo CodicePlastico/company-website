4765a9bae1833c1c9759ac69183b1481cdc1d91c
Davide <davide.brognoli@gmail.com>
2020-08-04 17:56:36 +0200

    Team mobile

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 import Spaceman from '../assets/team/spaceman.svg'
+import TeamGrid from '../components/team/teamGrid'
+import { useStaticQuery, graphql } from 'gatsby'
+
+import data from '../assets/team/team.yaml'
 
 const Team = () => {
+
+  const files = useStaticQuery(graphql`
+    query TeamQuery {
+      allFile(filter: {relativeDirectory: {eq: "team"}, ext: {eq: ".png"}}) {
+        nodes {
+          name
+          childImageSharp {
+            fluid {
+              src
+            }
+          }
+        }
+      }
+    }`
+  )
+
+  const teamMembers = data.team.map(m => {
+    const nodeImg = files.allFile.nodes.find(f => f.name === m.id)
+    const img = nodeImg ? nodeImg.childImageSharp.fluid.src : ''
+    return Object.assign({}, m, {img})
+  })
+
   return (
     <Layout>
       <SEO title="Team" />
@@ -15,10 +41,11 @@ const Team = () => {
           <div className="cp-grid__container">
             <div className="cp-grid__content">
               <h1><span>Il</span> Team</h1>
+              <TeamGrid team={teamMembers} />
             </div>
           </div>
         </div>
-        <div className="cp-grid cp-team__container">
+        <div className="cp-grid">
           <div className="cp-grid__left">
             <Spaceman />
           </div>

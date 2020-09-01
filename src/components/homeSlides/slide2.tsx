d62de51ab8c86959618f92a5bbd35b3af5cfd56a
Davide <davide.brognoli@gmail.com>
2020-09-01 11:54:58 +0200

    Slide 1

---
+++ src/components/homeSlides/slide2.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Slide2 = () => (
+  <div className="cp-home__slide">
+    <div className="cp-grid cp-home__grid">
+      <div className="cp-grid__container cp-home__grid-container">
+        <div className="cp-home__grid-content">
+          <h1>L'altra metà della <span>mela</span></h1>
+          <p>
+            Ogni progetto inizia con una <strong>stretta di mano</strong>, reale o virtuale poco importa.<br/> 
+            In CodicePlastico collaboriamo con aziende che condividono il nostro <strong>metodo</strong><br/>
+            e i nostri <strong>valori</strong>: conoscerci reciprocamente è importante.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default Slide2;

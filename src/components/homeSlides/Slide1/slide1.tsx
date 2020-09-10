20a1a7b43b46485bd23662e94239676ea12a1d01
Davide <davide.brognoli@gmail.com>
2020-09-10 13:59:46 +0200

    Slide 1

--- src/components/homeSlides/Slide1/slide1.tsx
+++ src/components/homeSlides/Slide1/slide1.tsx
@@ -4,6 +4,8 @@ import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 import { useScrollPosition } from '@n8tb1t/use-scroll-position'
 
 import stars from './stars.png';
+import planet1 from './planet_01.png';
+import planet2 from './planet_02.png';
 
 gsap.registerPlugin(MotionPathPlugin)
 
@@ -11,6 +13,12 @@ gsap.registerPlugin(MotionPathPlugin)
 const Slide1 = () => {
   const [tween, setTween] = useState(null)
   const [height, setHeight] = useState(0)
+  const [translate, setTranslate] = useState(0)
+
+  console.log(translate);
+  const style = {
+    transform: `translateZ(${translate}px)`
+  }
   
   useScrollPosition(({ currPos }) => {
     if (currPos.y, height) {
@@ -18,6 +26,7 @@ const Slide1 = () => {
       if (tween) {
         tween.progress(progress)
       }
+      setTranslate(Math.ceil(currPos.y * -50 / height));
     }
   })
   
@@ -46,13 +55,23 @@ const Slide1 = () => {
   }, []);
   
   return (
-    <div className="cp-home__slide cp-home__slide--1">
+    <div className="cp-home__slide cp-slide-1">
+      <div className="cp-home__background">
+        <img className="cp-slide-1__stars" src={stars} alt="stars" style={style}/>
+      </div>
       <div className="cp-home__animation">
-        <img id="ship" className="cp-home__animation-ship" src="/homeship.png" alt="ship" ref={onShipSet} />
+        <img id="ship" className="cp-slide-1__ship" src="/homeship.png" alt="ship" ref={onShipSet} />
         <svg width="100vh" height="100vh" viewBox="0 0 1424 1132" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path id="path" ref={onPathSet} d="M-74 10.6573C9.25962 -13.508 107.722 17.6151 185.498 46.4503C278.155 80.8026 355.466 134.566 422.674 201.553C507.835 286.434 574.278 393.917 600.456 506.697C611.182 552.91 623.818 599.533 634.936 646.072C658.146 743.234 702.044 831.311 780.031 902.046C863.81 978.034 951.988 1035.9 1067.03 1063.29C1183.98 1091.14 1305.9 1099.91 1422 1130" stroke="white" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" strokeDasharray="10 10"/>
         </svg>
-        <img src={stars} alt="stars"/>
+        <img className="cp-slide-1__planet1" src={planet1} alt="small planet"/>
+        <svg className="cp-slide-1__border1" width="158" height="158" viewBox="0 0 158 158" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="79" cy="79" r="78" stroke="white" stroke-width="2" stroke-dasharray="5 5"/>
+        </svg>
+        <img className="cp-slide-1__planet2" src={planet2} alt="big planet"/>
+        <svg className="cp-slide-1__border2" width="533" height="533" viewBox="0 0 533 533" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="266.5" cy="266.5" r="263.5" stroke="white" stroke-width="6" stroke-dasharray="20 20"/>
+        </svg>
       </div>
       <div className="cp-grid cp-home__grid">
         <div className="cp-grid__container cp-home__grid-container">
@@ -76,7 +95,7 @@ const Slide1 = () => {
           </div>
         </div>
       </div>
-      <div className="cp-home__footer cp-home__footer--1"></div>
+      <div className="cp-home__footer cp-slide-1__footer"></div>
     </div>
   )
 }

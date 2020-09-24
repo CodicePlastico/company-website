2d712edaaf655bd1e8bdb6eef18561c6e92b04a6
Davide <davide.brognoli@gmail.com>
2020-09-24 09:46:45 +0200

    Slide 4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import SEO from '../components/seo'
 import Slide1 from '../components/homeSlides/slide1/slide1'
 import Slide2 from '../components/homeSlides/slide2/slide2'
 import Slide3 from '../components/homeSlides/slide3/slide3'
+import Slide4 from '../components/homeSlides/slide4/slide4'
 
 const IndexPage = () => (
   <Layout>
@@ -13,6 +14,7 @@ const IndexPage = () => (
       <Slide1 />
       <Slide2 />
       <Slide3 />
+      <Slide4 />
     </div>
   </Layout>
 )

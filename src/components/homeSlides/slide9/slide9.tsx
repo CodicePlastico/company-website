39ce3d83d112b2b116c39555dce10faae387fe20
Davide <davide.brognoli@gmail.com>
2020-09-25 11:28:33 +0200

    Slide 9

---
+++ src/components/homeSlides/slide9/slide9.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Slide9 = () => {
+  return(
+    <div className="cp-home__slide cp-slide-7">
+      <div className="cp-grid cp-home__grid">
+        <div className="cp-grid__container">
+          <div className="cp-home__grid-content cp-home__grid-content--full">
+            <h1 className="cp-home__title">
+              <span>Il<br/><strong>traguardo</strong></span>
+            </h1>
+            <p>
+              Vi accompagniamo nel rilascio della piattaforma con attività di supporto
+              e formazione. Come per un’automobile, anche per il software è bene
+              definire piani di manutenzione e di feedback periodico: siamo sempre
+              pronti per nuove sfide, raccogliendo indicazioni e nuovi obiettivi,
+              nell’ottica di un miglioramento continuo del prodotto.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Slide9;

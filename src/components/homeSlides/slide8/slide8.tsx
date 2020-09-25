05132ca2f40ae8220d487c7c3e29c3c08e4cada7
Davide <davide.brognoli@gmail.com>
2020-09-25 11:26:39 +0200

    Slide 8

---
+++ src/components/homeSlides/slide8/slide8.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Slide8 = () => {
+  return(
+    <div className="cp-home__slide cp-slide-7">
+      <div className="cp-grid cp-home__grid">
+        <div className="cp-grid__container">
+          <div className="cp-home__grid-content cp-home__grid-content--full">
+            <h1 className="cp-home__title">
+              <span>Quando le cose<br/><strong>cambiano</strong></span>
+            </h1>
+            <p>
+              Da questo momento le attività di design, sviluppo, rilascio e test si
+              ripetono ciclicamente: ogni volta che una piccola porzione del software
+              è funzionante, la rendiamo disponibile. Ad ogni iterazione il progetto
+              cresce, arricchendosi di funzionalità e di dettagli. Rilasciare
+              soluzioni intermedie “grezze” ha un duplice vantaggio: ci consente di
+              essere veloci e di gestire il cambiamento. I vostri feedback e i test
+              sono fondamentali per individuare funzionalità superflue e tracciare
+              nuove necessità.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Slide8;

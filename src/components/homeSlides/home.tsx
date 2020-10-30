36246e610b0ca8f480178ed67e2fabdf69016fbf
Davide <davide.brognoli@gmail.com>
2020-10-30 14:54:18 +0100

    Home mobile

---
+++ src/components/homeSlides/home.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Slide1 from './slide1/slide1'
+import Slide2 from './slide2/slide2'
+import Slide3 from './slide3/slide3'
+import Slide4 from './slide4/slide4'
+import Slide7 from './slide7/slide7'
+import Slide8 from './slide8/slide8'
+import Slide9 from './slide9/slide9'
+
+const Home = () => (
+  <div className="cp-home">
+    <Slide1 />
+    <Slide2 />
+    <Slide3 />
+    <Slide4 />
+    <Slide7 />
+    <Slide8 />
+    <Slide9 />
+  </div>
+)
+
+export default Home;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,46 @@
-import React from 'react'
-import { BrowserView, MobileView } from 'react-device-detect';
+import React, { useState } from 'react'
+import { isBrowser, isMobile } from 'react-device-detect';
+import loadable from '@loadable/component'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import Slide1 from '../components/homeSlides/slide1/slide1'
-import Slide2 from '../components/homeSlides/slide2/slide2'
-import Slide3 from '../components/homeSlides/slide3/slide3'
-import Slide4 from '../components/homeSlides/slide4/slide4'
-import Slide7 from '../components/homeSlides/slide7/slide7'
-import Slide8 from '../components/homeSlides/slide8/slide8'
-import Slide9 from '../components/homeSlides/slide9/slide9'
-import HomeMobile from '../components/homeMobile/homeMobile';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <MobileView>
-      <HomeMobile />
-    </MobileView>
-    <BrowserView>
-      <Slide1 />
-      <Slide2 />
-      <Slide3 />
-      <Slide4 />
-      <Slide7 />
-      <Slide8 />
-      <Slide9 />
-    </BrowserView>
-  </Layout>
-)
+const Mobile = loadable(() => import('../components/homeMobile/homeMobile'))
+const Home = loadable(() => import('../components/homeSlides/home'))
+
+type HomeView = 'unknown' | 'mobile' | 'browser';
+
+const getContent = (view: HomeView) => {
+  console.log(view);
+  switch(view) {
+    case 'mobile':
+      return <Mobile />;
+    case 'browser':
+      return <Home />;
+    default:
+      return <></>;
+  }
+}
+
+const IndexPage = () => {
+  const [view, setView] = useState<HomeView>('unknown');
+
+  useState(() => {
+    if (isMobile) {
+      setView('mobile');
+    } else if (isBrowser) {
+      setView('browser');
+    }
+  })
+
+  const content = getContent(view);
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {content}
+    </Layout>
+  )
+}
 
 export default IndexPage

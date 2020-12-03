f53e6f3f31df12fe96d571145550f61b671c6cfe
Andrea Gadaldi <ilgadaldi@gmail.com>
2020-12-03 18:04:51 +0100

    WIP screen

--- src/components/homeSlides/slide9/slide9.tsx
+++ src/components/homeSlides/slide9/slide9.tsx
@@ -12,14 +12,24 @@ const Slide9 = () => {
             <h1 className="cp-home__title cp-slide-9__title">
               <span>Il <strong>traguardo</strong></span>
             </h1>
+            <div className="cp-slide-9__animation-line-1">
+              <svg>
+                <path d="M0 0 l200 0" stroke="#ffffff" strokeWidth="2" strokeDasharray="5 5"/>
+              </svg>
+            </div>
+            <div className="cp-slide-9__animation-line-2">
+              <svg>
+                <path d="M0 0 l200 0" stroke="#ffffff" strokeWidth="2" strokeDasharray="5 5"/>
+              </svg>
+            </div>
             <div className="cp-slide-9__animation-circle-1">
               <svg viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle r="200" transform="matrix(-1 0 0 1 200 200)" stroke="#ffffff" strokeWidth="5" strokeDasharray="15 15"/>
+                <circle r="200" transform="matrix(-1 0 0 1 200 200)" stroke="#ffffff" strokeWidth="3" strokeDasharray="10 10"/>
               </svg>
             </div>
             <div className="cp-slide-9__animation-circle-2">
-              <svg viewBox="0 0 500 500" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle r="250" transform="matrix(-1 0 0 1 250 250)" stroke="#ffffff" strokeWidth="3" strokeDasharray="10 10"/>
+              <svg viewBox="0 0 600 600" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle r="300" transform="matrix(-1 0 0 1 300 300)" stroke="#ffffff" strokeWidth="3" strokeDasharray="10 10"/>
               </svg>
             </div>
             <img src={screen} alt="screen" />

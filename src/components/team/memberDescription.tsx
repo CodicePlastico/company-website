839e53cee70206e6a640fa9de012372d0ae070d3
Andrea Gadaldi <ilgadaldi@gmail.com>
2021-01-22 09:16:37 +0100

    Fix team close button

--- src/components/team/memberDescription.tsx
+++ src/components/team/memberDescription.tsx
@@ -17,7 +17,9 @@ const MemberDescription = (props: MemberDescriptionProps) => {
   const descriptionClass = classNames('cp-member__description', {'cp-member__description--visible': visible}, {'cp-member__description--right': right})
   return (
     <div className={descriptionClass} style={memberStyle} onClick={handleClick}>
-        <button className="cp-member__description-close" onClick={toggleDescription}>x</button>
+        <button className="cp-member__description-close" onClick={toggleDescription}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>
+        </button>
         <div className="cp-member__description-info">
           <h5 className="cp-member__description-title">{member.name}</h5>
           <p className="cp-member__description-extras"><strong>{member.nick && <>@{member.nick} - </>}{member.role}</strong></p>

ad3e07f192237c27274e353b011f1757b855a9f2
Andrea Gadaldi <ilgadaldi@gmail.com>
2021-01-25 16:13:34 +0100

    cookie consent

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
+import CookieConsent from 'react-cookie-consent';
 
 import Header from './header'
 import Footer from './footer'
@@ -26,6 +27,14 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <CookieConsent
+        location="bottom"
+        buttonText="Accetto"
+        cookieName="gatsby-gdpr-google-tagmanager"
+        style={{ background: "#D93232" }}
+        buttonStyle={{ background: "#111D24", color: "#fff", fontSize: "13px" }}>
+        Questo sito usa i cookie, i biscotti sono buoni ma è sempre meglio conoscere cosa c'è dentro per non incappare in strane sorprese. Se vuoi sapere quali sono i nostri ingredienti, <a style={{ color: "#111D24"}} href="www.google.com" target="_blank">clicca qui</a>.
+      </CookieConsent>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div>
         <main>{children}</main>

d02b30e7a03e2d912d934f6292a7fe6a5f184e53
Andrea Gadaldi <ilgadaldi@gmail.com>
2021-01-27 15:32:46 +0100

    Fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,27 +54,5 @@ module.exports = {
         offset: -100
       }
     },
-    {
-      resolve: `gatsby-plugin-gdpr-cookies`,
-      options: {
-        googleAnalytics: {
-          trackingId: '', // leave empty if you want to disable the tracker
-          cookieName: 'gatsby-gdpr-google-analytics', // default
-          anonymize: true, // default
-          allowAdFeatures: false // default
-        },
-        googleTagManager: {
-          trackingId: 'GTM-KZ49XNH', // leave empty if you want to disable the tracker
-          cookieName: 'gatsby-gdpr-google-tagmanager', // default
-          dataLayerName: 'dataLayer', // default
-        },
-        facebookPixel: {
-          pixelId: '', // leave empty if you want to disable the tracker
-          cookieName: 'gatsby-gdpr-facebook-pixel', // default
-        },
-        // defines the environments where the tracking should be available  - default is ["production"]
-        environments: ['production', 'development']
-      },
-    },
   ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
-import CookieConsent from 'react-cookie-consent';
 
 import Header from './header'
 import Footer from './footer'
@@ -27,18 +26,6 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <CookieConsent
-        location="bottom"
-        buttonText="Accetto"
-        enableDeclineButton
-        declineButtonText="Non accetto"
-        cookieName="gatsby-gdpr-google-tagmanager"
-        style={{ background: "#D93232" }}
-        SameSite="none"
-        Secure
-        buttonStyle={{ background: "#111D24", color: "#fff", fontSize: "13px" }}>
-        <span>Questo sito usa i cookie, i biscotti sono buoni ma è sempre meglio conoscere cosa c'è dentro per non incappare in strane sorprese. Se vuoi sapere quali sono i nostri ingredienti, <a style={{ color: "#111D24"}} href="https://www.iubenda.com/privacy-policy/96999165/legal" target="_blank" rel="noreferrer">clicca qui</a>.</span>
-      </CookieConsent>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div>
         <main>{children}</main>

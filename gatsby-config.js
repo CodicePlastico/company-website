02da4e123c914cd2d15a0420187687c9b0b2b611
Davide <davide.brognoli@gmail.com>
2021-01-27 17:48:39 +0100

    Add path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 const path = require('path'); 
 
 module.exports = {
+  pathPrefix: 'company-website',
   siteMetadata: {
     title: `CodicePlastico`,
     description: `CodicePlastico è un grande piccolo team di software engineer`,

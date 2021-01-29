7e9af74c36bef1c1b1db9dad44f7713ce4f9b4a5
Andrea Gadaldi <ilgadaldi@gmail.com>
2021-01-29 12:18:26 +0100

    various fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   siteMetadata: {
     title: `CodicePlastico`,
+    siteUrl: `https://www.codiceplastico.com`,
     description: `CodicePlastico è un grande piccolo team di software engineer`,
     author: `@gatsbyjs`,
   },
@@ -58,28 +59,10 @@ module.exports = {
       resolve: "gatsby-plugin-google-tagmanager",
       options: {
         id: "GTM-KZ49XNH",
-  
-        // Include GTM in development.
-        //
-        // Defaults to false meaning GTM will only be loaded in production.
+
         includeInDevelopment: false,
-  
-        // datalayer to be set before GTM is loaded
-        // should be an object or a function that is executed in the browser
-        //
-        // Defaults to null
+
         defaultDataLayer: { platform: "gatsby" },
-  
-        // Specify optional GTM environment details.
-        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-        dataLayerName: "YOUR_DATA_LAYER_NAME",
-  
-        // Name of the event that is triggered
-        // on every Gatsby route change.
-        //
-        // Defaults to gatsby-route-change
-        routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
       },
     },
     {
@@ -105,39 +88,10 @@ module.exports = {
               "customizeButtonColor": "#DADADA",
               "customizeButtonCaptionColor": "#4D4D4D" 
           },
-          "callback": {
-              onPreferenceExpressedOrNotNeeded: function(preference) {
-                  dataLayer.push({
-                      iubenda_ccpa_opted_out: _iub.cs.api.isCcpaOptedOut()
-                  });
-                  if (!preference) {
-                      dataLayer.push({
-                          event: "iubenda_preference_not_needed"
-                      });
-                  } else {
-                      if (preference.consent === true) {
-                          dataLayer.push({
-                              event: "iubenda_consent_given"
-                          });
-                      } else if (preference.consent === false) {
-                          dataLayer.push({
-                              event: "iubenda_consent_rejected"
-                          });
-                      } else if (preference.purposes) {
-                          for (var purposeId in preference.purposes) {
-                              if (preference.purposes[purposeId]) {
-                                  dataLayer.push({
-                                      event: "iubenda_consent_given_purpose_" + purposeId
-                                  });
-                              }
-                          }
-                      }
-                  }
-              }
-          }
         },
         googleTagManagerOptions: true
       }
-    }
+    },
+    `gatsby-plugin-sitemap`
   ],
 }

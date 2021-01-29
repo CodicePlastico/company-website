5a564192f166e12791fb8258dd1d465e0a5546c6
Andrea Gadaldi <ilgadaldi@gmail.com>
2021-01-29 12:30:19 +0100

    it vs en

---
+++ src/pages/en.tsx
@@ -0,0 +1,14 @@
+import Layout from '../components/layout'
+
+import Home from '../components/home/home'
+
+const En = () => {
+
+  return (
+    <Layout>
+        <Home/>
+    </Layout>
+  )
+}
+
+export default En

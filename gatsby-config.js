fb5d7746b5221d57ec97cc896842ba4c135bee75
Andrea Gadaldi <ilgadaldi@gmail.com>
2021-01-29 11:41:24 +0100

    Iubenda per chi non si accontenda

--- gatsby-config.js
+++ gatsby-config.js
@@ -105,36 +105,36 @@ module.exports = {
               "customizeButtonColor": "#DADADA",
               "customizeButtonCaptionColor": "#4D4D4D" 
           },
-          "callback": {
-              onPreferenceExpressedOrNotNeeded: function(preference) {
-                  dataLayer.push({
-                      iubenda_ccpa_opted_out: _iub.cs.api.isCcpaOptedOut()
-                  });
-                  if (!preference) {
-                      dataLayer.push({
-                          event: "iubenda_preference_not_needed"
-                      });
-                  } else {
-                      if (preference.consent === true) {
-                          dataLayer.push({
-                              event: "iubenda_consent_given"
-                          });
-                      } else if (preference.consent === false) {
-                          dataLayer.push({
-                              event: "iubenda_consent_rejected"
-                          });
-                      } else if (preference.purposes) {
-                          for (var purposeId in preference.purposes) {
-                              if (preference.purposes[purposeId]) {
-                                  dataLayer.push({
-                                      event: "iubenda_consent_given_purpose_" + purposeId
-                                  });
-                              }
-                          }
-                      }
-                  }
-              }
-          }
+          // "callback": {
+          //     onPreferenceExpressedOrNotNeeded: function(preference) {
+          //         dataLayer.push({
+          //             iubenda_ccpa_opted_out: _iub.cs.api.isCcpaOptedOut()
+          //         });
+          //         if (!preference) {
+          //             dataLayer.push({
+          //                 event: "iubenda_preference_not_needed"
+          //             });
+          //         } else {
+          //             if (preference.consent === true) {
+          //                 dataLayer.push({
+          //                     event: "iubenda_consent_given"
+          //                 });
+          //             } else if (preference.consent === false) {
+          //                 dataLayer.push({
+          //                     event: "iubenda_consent_rejected"
+          //                 });
+          //             } else if (preference.purposes) {
+          //                 for (var purposeId in preference.purposes) {
+          //                     if (preference.purposes[purposeId]) {
+          //                         dataLayer.push({
+          //                             event: "iubenda_consent_given_purpose_" + purposeId
+          //                         });
+          //                     }
+          //                 }
+          //             }
+          //         }
+          //     }
+          // }
         },
         googleTagManagerOptions: true
       }

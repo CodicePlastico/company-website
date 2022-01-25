186aee2f2a656600d94c31fb1e5508c45bee2c04
Andrea Gadaldi <ilgadaldi@gmail.com>
2022-01-25 09:04:44 +0100

    Updates

--- component---src-pages-plastic-blog-tsx-7df1f16c90999a7f0437.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{u7i3:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),c=n("Wbzz"),o=n("Bl7J"),r=n("vrFN");t.default=function(){return l.a.createElement(o.a,null,l.a.createElement(r.a,{title:"Plastic Blog"}),l.a.createElement("h1",null,"Plastic Blog"),l.a.createElement(c.Link,{to:"/"},"Go to home")," ",l.a.createElement("br",null))}}}]);
-//# sourceMappingURL=component---src-pages-plastic-blog-tsx-7df1f16c90999a7f0437.js.map

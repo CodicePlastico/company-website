1afb0a7c62876da0d20bfb8ff854d97de0489f8a
Mariachiara Pezzotti <mariachiara.pezzotti@gmail.com>
2023-04-26 16:59:35 +0200

    Allestimento CH - Pt.1

---
+++ src/pages/ch-finance.tsx
@@ -0,0 +1,207 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+
+
+const ChFinance = () => (
+  <Layout>
+    <SEO title="Case History - Software per Intermediatori Finanziari" />
+    <div className="cp-internal-page cp-ch cp-ch-finance ">
+      <div className="cp-internal-page__bg"></div>
+
+      <div className="cp-internal-page__content cp-internal-page__content--bg--design cp-grid intro">
+        <div className="cp-grid__container">
+          <div className="cp-internal-page__content cp-grid__content">
+            <h1><span>Case</span> History</h1>
+            <h2 className="">
+              Come abbiamo aiutato aziende del <span>settore finanziario</span> a digitalizzare i loro processi
+            </h2>
+
+            <h5><span> Da molti anni sviluppiamo software per il settore Finanziario e di Risk Management.</span></h5>
+
+            <p>Dalla Fintech che gestisce migliaia di pratiche al giorno alle piccole società di servizi, i problemi
+              che le aziende di questo tipo devono affrontare quando avviano un processo di trasformazione digitale sono simili: <strong>raccolta dati</strong> da sistemi esterni come <em>Aida, Cerved, Telemaco o altri servizi</em>,
+              analizzare <strong>documenti</strong>, <strong>bilanci</strong> e fare <strong>calcoli</strong>, ma soprattutto razionalizzare le procedure abbandonando i tanti amati fogli di calcolo.
+            </p><p>Ma partiamo dal principio.</p>
+
+          </div>
+        </div>
+
+      </div>
+
+      <div className='cp-internal-page__content cp-grid cp-bg-inverted__maincolor'>
+        <div className="cp-grid__container">
+          <div className='cp-grid__content'>
+            <h2> La situazione iniziale</h2>
+            <p className='emph'>
+              In principio c’era la carta. Poi sono arrivate le email, i fogli Excel e i documenti in PDF.<br />In principio i team erano di poche persone in grado di organizzarsi. Poi l’azienda si è ingrandita in breve tempo.
+            </p>
+            <p>Le aziende di servizi, soprattutto se operano in ambiti regolati, devono garantire alti standard di affidabilità e sicurezza. Non sempre, però, la crescita aziendale va di pari passo con l’evoluzione degli strumenti operativi. Questi sono i macro problemi che abbiamo raccolto nella nostra esperienza:</p>
+            <div className='cp-ch__row r1'>
+             
+              {/* inizio block */}
+              <div className='cp-ch__block'>
+                <div className='cp-ch__icon'>
+                  <img src='/ch/finance/training.svg' alt="icon" />
+                </div>
+                <p><strong>Le procedure non sono ben documentate</strong>: molte abilità dipendono da
+                  alcuni dipendenti storici (silos di conoscenza), molte procedure sono gestite tramite
+                  fogli di calcolo condivisi, <strong className='inverted'>difficili da manutenere</strong> e su cui 
+                  <strong className='inverted'> non c’è modo di tracciare chi ha fatto cosa</strong>.</p>
+              </div>
+              {/* fine block */}
+            
+            {/* inizio block */}
+            <div className='cp-ch__block'>
+                <div className='cp-ch__icon'>
+                  <img src='/ch/finance/knowledge.svg' alt="icon" />
+                </div>
+                <p><strong>L’operatività è frammentata su molti strumenti</strong>: il CRM per il contatto, il vecchio gestionale per
+                 la fatturazione, Cerved per scaricare un report, Excel per fare i conti e 
+                 via dicendo. Quali strumenti, in che modo usarli, con quali credenziali è tutta <strong className='inverted'>operatività a carico degli operatori</strong>.</p>
+              </div>
+              {/* fine block */}
+            
+            </div>
+
+            <div className='cp-ch__row r2'>
+
+              {/* inizio block */}
+              <div className='cp-ch__block'>
+                <div className='cp-ch__icon'>
+                  <img src='/ch/finance/file.svg' alt="icon" />
+                </div>
+                <p>Nel settore finanziario la <strong>raccolta documentale</strong> è un tema molto presente: finanziamenti, pratiche, bandi vanno a buon fine quando vengono raccolti documenti giusti, con le informazioni giuste. </p>
+<p><strong className="inverted">Spesso le comunicazioni e gli scambi di documenti con i clienti finali avvengono via email. &Egrave; facile perderli.</strong></p>
+              </div>
+              {/* fine block */}
+
+              {/* inizio block */}
+              <div className='cp-ch__block'>
+                <div className='cp-ch__icon'>
+                  <img src='/ch/finance/distributed.svg' alt="icon" />
+                </div>
+                <p>
+                L’<strong>archiviazione</strong> dei documenti raccolti avviene su server condivisi, con <strong className='inverted'>convenzioni basate sui nomi</strong> dei file e delle cartelle. &Egrave; facile sbagliare. 
+                </p>
+              </div>
+              {/* fine block */}
+
+              {/* inizio block */}
+              <div className='cp-ch__block'>
+                <div className='cp-ch__icon'>
+                  <img src='/ch/finance/stamp.svg' alt="icon" />
+                </div>
+                <p>Le <strong>validazioni</strong> delle informazioni vengono eseguite <strong className='inverted'>manualmente</strong> dagli operatori. Ci vuole tempo.</p>
+              </div>
+              {/* fine block */}
+
+            
+
+            </div>
+
+          </div>{/* fine content*/}
+        </div>{/* fine container*/}
+      </div>
+
+      <div className="cp-grid cp-service__research">
+        <div className="cp-grid__left">
+        </div>
+        <div className="cp-grid__container">
+          <div className="cp-grid__content">
+            <h4><span>Ricerca e analisi</span></h4>
+            <p>
+              Organizziamo <strong>workshop partecipativi</strong> per l’analisi di processi esistenti
+              o per la creazione di nuove applicazioni. Attraverso queste attività diamo forma alle
+              necessità degli <strong>utenti</strong>, strutturiamo le <strong>informazioni</strong>
+              e studiamo le interazioni
+              necessarie in armonia con obiettivi e modelli di business.
+            </p>
+            <p>
+              Crediamo molto nel coinvolgimento di utenti e stakeholders:
+              la condivisione della progettazione sotto forma di <strong>gioco</strong> consente
+              sia di <strong>creare soluzioni concrete</strong> e tagliate sugli utenti
+              sia di far <strong>emergere problemi e criticità</strong>.
+            </p>
+            <p>
+              Individuare punti di vista diversi, pain points e veri e propri dubbi è uno
+              degli aspetti <strong>strategici</strong> dell’approccio partecipativo:
+              permette di <strong>valutare idee prima di investire</strong> in design e sviluppo,
+              trovando delle soluzioni, alternative di strategia e azioni concrete.
+            </p>
+            <p>
+              <br />
+              <a href="/contatti/" className="cp-service__button--bordered ">Hai un’idea e non sai da dove partire?</a>
+            </p>
+
+          </div>
+        </div>
+      </div>
+
+      <div className="cp-grid cp-service__separator">
+        <div className="cp-grid__container">
+          <div className="cp-grid__content">
+            <p>
+              qui
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="cp-grid cp-service__uxui">
+
+        <div className="cp-grid__left">
+        </div>
+        <div className="cp-grid__container">
+          <div className="cp-grid__content">
+            <h4><span>UX & UI</span></h4>
+            <p>
+              Una volta definite le necessità ci occupiamo della <strong>progettazione</strong> dell’interfaccia.
+              Il risultato è un artefatto che consente ai nostri clienti di <strong>valutare</strong> aspetto,
+              interazioni, microtesti e in generale il look & feel dell’applicazione.
+            </p>
+            <p>
+              Anche in questa fase l’approccio può essere <strong>partecipativo</strong> con sessioni di
+              rapid prototyping che, successivamente, vengono normalizzate e ridisegnate dal nostro team di design.
+            </p>
+            <h5>Realizzazione prototipi: gradi di fedeltà e interazioni</h5>
+            <p>Per grado di fedeltà di un prototipo si intende quanto il prototipo assomiglia al
+              prodotto finito. Realizziamo sia prototipi a <strong>bassa fedeltà</strong> (es. wireframe)
+              che consentono di testare flussi e interazioni di alto livello,
+              sia prototipi ad <strong>alta fedeltà</strong> per valutare in maniera precisa
+              l’aspetto dell’applicazione.
+            </p>
+            <p>
+              I prototipi interattivi consentono di <strong>valutare scenari di interazione</strong> attraverso
+              l’utilizzo del prototipo in un ambiente di simulazione (pc, tablet o smartphone).
+              Questo tipo di servizio è particolarmente indicato sia per <strong>testare funzionalità specifiche</strong> con gli utenti, sia per <strong>presentazioni</strong> di alto profilo.
+            </p>
+            <p><br />
+              <a href="/contatti/" className="cp-service__button--bordered ">Inizia a progettare con noi</a>
+            </p>
+
+          </div>
+        </div>
+      </div>
+
+      <div className="cp-grid cp-service__tools">
+        <div className="cp-grid__container">
+          <div className="cp-grid__content">
+            <p><strong>CHE STRUMENTI USIAMO?</strong>: Carta, penna, postit, colla e Figma</p>
+          </div>
+        </div>
+      </div>
+      <div className="cp-grid cp-service__innermenu">
+        <div className="cp-grid__container">
+          <div className="cp-grid__content">
+            <p><a href="/about/"><strong>About</strong></a> - <a href="/design/">Design</a>  - <a href="/dev/">Dev</a> - <a href="/devops/">Devops & Cloud</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Layout>
+)
+
+export default ChFinance

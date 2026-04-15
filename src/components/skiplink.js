cc7e47ad4c17b0a4de270b30d15dde111efcd5c6
Moreno-Gentili <moreno.gentili@codiceplastico.com>
2026-04-15 11:55:22 +0200

    Fix classname

--- src/components/skiplink.js
+++ src/components/skiplink.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const SkipLink = () => (
-  <a className="skiplink" href="#content">Vai al contenuto</a>
+  <a className="cp-header__skiplink" href="#content">Vai al contenuto</a>
 )
 
 export default SkipLink
